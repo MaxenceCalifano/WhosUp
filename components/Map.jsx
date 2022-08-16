f355d69a5a5de8042419f3f4f88fb19de274e5cc
Maxence <maxence.califano@gmail.com>
2022-08-16 14:30:54 +0200

    build: particpants slider

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,54 @@
 
 import React from 'react';
+import { View, Text } from 'react-native'
 import MapView from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 
+import * as Location from 'expo-location';
+
+import '../config/firebase'
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
+const firestore = getFirestore();
+
 export default function Map() {
+
+    const [location, setLocation] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+
+    React.useEffect(() => {
+
+
+        (async () => {
+
+            await setDoc(doc(firestore, "characters", "mario"), {
+                employment: "plumber",
+                outfitColor: "red",
+                specialAttack: "fireball"
+            });
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            console.log(location.coords.latitude)
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
     return (
-        <MapView style={styles.map} />
+        <View>
+            <MapView style={styles.map} />
+            <Text>{text}</Text>
+        </View>
     )
 }
 
--- screens/AddEventScreen.jsx
+++ screens/AddEventScreen.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { View, Text } from 'react-native'
+import { Input, Slider, Icon } from 'react-native-elements'
+
+import styles from "../styles";
 
 
 export default function AddEventScreen() {
+    const [people, setPeople] = React.useState(1);
+
     return (
         <View style={{ flex: 1 }}>
-            <Text>Page ajouter un événement</Text>
+
+            <Input placeholder="Titre de l'activité" />
+            <Text>Nombre de participants: {people}</Text>
+
+            <Slider
+                value={people}
+                onValueChange={setPeople}
+                maximumValue={10}
+                minimumValue={1}
+                step={1}
+                thumbStyle={{ height: 30, width: 30, backgroundColor: styles.color, justifyContent: 'center' }}
+                thumbProps={{
+                    children: (
+                        <Icon
+                            name="people"
+                            type="material"
+                            size={20}
+                            color="#757575"
+                            containerStyle={{ bottom: 0, right: 0 }}
+                        />
+                    )
+                }}
+            />
         </View>
     );
 }
