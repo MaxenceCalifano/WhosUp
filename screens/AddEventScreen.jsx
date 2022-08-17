97f502e1c4851e6e83dffd3585871f69815aedc6
Maxence <maxence.califano@gmail.com>
2022-08-17 08:03:18 +0200

    build: activity picker

--- screens/AddEventScreen.jsx
+++ screens/AddEventScreen.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { View, Text } from 'react-native'
-import { Input, Slider, Icon } from 'react-native-elements'
+import { Input, Slider, Icon, ListItem } from 'react-native-elements'
+import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import styles from "../styles";
 
 
 export default function AddEventScreen() {
     const [people, setPeople] = React.useState(1);
+
+    const [selectedActivityType, setSelectedActivityType] = React.useState();
+
+    const eventTypes = [
+        'randonée',
+        'jeux de société',
+        'sport',
+        'apéro',
+    ]
 
     return (
         <View style={{ flex: 1 }}>
@@ -33,6 +44,16 @@ export default function AddEventScreen() {
                     )
                 }}
             />
+
+            <Picker
+                selectedValue={selectedActivityType}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedActivityType(itemValue)
+                }>
+                <Picker.Item label="jeux de société" value="jeux de société" />
+                <Picker.Item label="apéro" value="apéro" />
+                <Picker.Item label="randonée" value="randonée" />
+            </Picker>
         </View>
     );
 }
