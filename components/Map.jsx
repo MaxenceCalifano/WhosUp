604e31feac8e8a35e64bfd2d3737c9d8d85c7e3b
Maxence <maxence.califano@gmail.com>
2022-11-30 09:15:17 +0100

    diplay markers

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { View, Text } from 'react-native'
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 
 import * as Location from 'expo-location';
@@ -14,30 +15,23 @@ export default function Map() {
 
     const [location, setLocation] = React.useState(null);
     const [errorMsg, setErrorMsg] = React.useState(null);
+    const [markers, setMarkers] = React.useState([]);
 
     const fetchActivities = async () => {
         const activities = await getDocs(collection(db, "activities"))
-        activities.forEach(doc => console.log(doc.data()))
+        const allMarkers = []
+        activities.forEach(doc => {
+            //console.log(doc.data())
+            //setMarkers(prevState => prevState.push(doc.data()))
+            // console.log(markers)
+            allMarkers.push(doc.data())
+        })
 
-        //console.log(test)
+        setMarkers(allMarkers)
     }
 
     React.useEffect(() => {
-        fetchActivities()
-
-        /* (async () => {
-
-
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-            console.log(location.coords.latitude)
-        })(); */
+        fetchActivities()//.then(console.log("markers", markers))
     }, []);
 
     let text = 'Waiting..';
@@ -46,11 +40,28 @@ export default function Map() {
     } else if (location) {
         text = JSON.stringify(location);
     }
+
+    const returnParsedMarker = (marker) => {
+        const latLng = {
+            latitude: parseInt(marker.location.latitude),
+            longitude: parseInt(marker.location.longitude)
+        }
+
+    }
+
     return (
-        <View>
-            <MapView style={styles.map} />
+        < View >
+            <MapView style={styles.map}>
+                {markers.map((marker, index) => (
+
+                    <Marker key={index}
+                        coordinate={marker.location}
+                        title={marker.activityTitle}
+                        description={marker.activityDescription} />
+                ))}
+            </ MapView>
             <Text>{text}</Text>
-        </View>
+        </View >
     )
 }
 
