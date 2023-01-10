0650c1d7a696941ff6c31115ce1bf22c20542762
Maxence <maxence.califano@gmail.com>
2023-01-10 21:05:36 +0200

    build: carousel and handle gesture on it

--- components/Map.jsx
+++ components/Map.jsx
@@ -5,6 +5,7 @@ import MapView from 'react-native-maps';
 import { Marker, Callout } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import * as Location from 'expo-location';
 
@@ -52,63 +53,69 @@ export default function Map() {
     }
 
     return (
-        < View >
-            <MapView style={styles.map}>
-                {markers.map((marker, index) => {
-                    console.log(marker.a)
-                    if (marker.activityType == "randonée") {
-                        return (
-                            < Marker key={index}
-                                coordinate={marker.location}
-                                title={marker.activityTitle}
-                                description={marker.activityDescription}
-                                image={require('../assets/hike.png')}
-                            />
-                        )
-                    } if (marker.activityType == "jeux de société") {
-                        return (
-                            < Marker key={index}
-                                coordinate={marker.location}
-                                title={marker.activityTitle}
-                                description={marker.activityDescription}
-                                image={require('../assets/games.png')}
-                            />
-                        )
-                    }
-                })}
+        <GestureHandlerRootView>
+            < View >
+                <MapView style={styles.map}>
+                    {markers.map((marker, index) => {
+                        console.log(marker.a)
+                        if (marker.activityType == "randonée") {
+                            return (
+                                < Marker key={index}
+                                    coordinate={marker.location}
+                                    title={marker.activityTitle}
+                                    description={marker.activityDescription}
+                                    image={require('../assets/hike.png')}
+                                />
+                            )
+                        } if (marker.activityType == "jeux de société") {
+                            return (
+                                < Marker key={index}
+                                    coordinate={marker.location}
+                                    title={marker.activityTitle}
+                                    description={marker.activityDescription}
+                                    image={require('../assets/games.png')}
+                                />
+                            )
+                        }
+                    })}
 
-            </ MapView>
-            <Carousel
-                loop
-                width={500}
-                height={500 / 2}
-                autoPlay={false}
-                data={[...new Array(6).keys()]}
-                scrollAnimationDuration={1000}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                            position: 'absolute',
-                            top: 150,
-                            zIndex: 10000,
-                            backgroundColor: 'red',
-                            left: 150
-                        }}
-                    >
-                        <Text style={{
-                            textAlign: 'center', fontSize: 30, position: 'absolute', top: 250
-                        }}>
-                            {index}
-                        </Text>
-                    </View>
-                )}
-            />
-            <Text>{text}</Text>
-        </View >
+                </ MapView>
+                <Carousel
+                    style={styles.carousel}
+                    loop
+                    width={Dimensions.get('window').width}
+                    height={100}
+                    autoPlay={false}
+                    data={[...new Array(6).keys()]}
+                    scrollAnimationDuration={1000}
+                    onSnapToItem={(index) => console.log('current index:', index)}
+                    renderItem={({ index }) => (
+                        <View
+                            style={{
+                                flex: 1,
+                                borderWidth: 1,
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Text style={{
+                                textAlign: 'center', fontSize: 30
+                            }}>
+                                {index}
+                            </Text>
+                        </View>
+                    )}
+                />
+
+                <Text style={{
+                    position: 'absolute',
+                    /*  top: 150,
+                     zIndex: 10000,
+                     backgroundColor: 'red',
+                     left: 150 */
+                }}>{text}</Text>
+            </View >
+        </GestureHandlerRootView>
+
     )
 }
 
@@ -117,4 +124,10 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
     },
+    carousel: {
+        position: 'absolute',
+        top: Dimensions.get('window').height - 500,
+        backgroundColor: 'red',
+        left: 0
+    }
 });
