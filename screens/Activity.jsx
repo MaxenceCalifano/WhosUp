a97f5df5b72168c4bcdd17d0b100ea78bfa61afe
Maxence <maxence.califano@gmail.com>
2023-02-13 22:44:59 +0200

    fix: double subscription

--- screens/Activity.jsx
+++ screens/Activity.jsx
@@ -1,47 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 import { useAuthentication } from '../utils/hooks/useAuthentication'
 import '../config/firebase'
-import { getFirestore, updateDoc, doc } from 'firebase/firestore';
-const firestore = getFirestore();
+import { getFirestore, updateDoc, doc, onSnapshot } from 'firebase/firestore';
+import app from '../config/firebase';
+const firestore = getFirestore(app);
 
-//"Test_a5dd1a08-cb9a-46ac-a288-c8fc430423c8"
 
 function Activity({ route, navigation }) {
+
+    const [participateMessage, setParticipateMessage] = useState()
 
     // Will provide informations from the user that request participating
     const { user } = useAuthentication()
 
+    let { item } = route.params;
 
-    const { item } = route.params;
-    let newApplicantsArray = []
-    useEffect(() => {
-        if (item && user) {
-            newApplicantsArray = [...item.applicants]
-            newApplicantsArray.push({
-                userID: user.uid,
-                displayName: user.providerData[0].displayName,
-                photoURL: user.photoURL,
-            })
-        }
+    onSnapshot(doc(firestore, "activities", item.id), (doc) => {
+        item = { ...doc.data(), id: doc.id }
+    });
 
-    })
+    const userHasAlreadyApplied = (arr) => (
+        arr.some((elem) => elem.userID === user.uid)
+    )
 
     const participate = async () => {
-        console.log('vous avez demandé à participer à cette activité')
-        const activityRef = doc(firestore, "activities", item.id)
+        let newApplicantsArray = []
 
+        if (user) {
+            newApplicantsArray = [...item.applicants]
+            if (userHasAlreadyApplied(item.applicants)) {
+                console.log('existe déja')
+                setParticipateMessage("Vous avez déjà demandé à participer et votre demande a bien été envoyée")
+            } else {
+                newApplicantsArray.push({
+                    userID: user.uid,
+                    displayName: user.providerData[0].displayName,
+                    photoURL: user.photoURL,
+                })
 
-        try {
-            await updateDoc(activityRef, {
-                applicants: newApplicantsArray
-            })
-                .then(() => console.log("c'est bon"))
+                const activityRef = doc(firestore, "activities", item.id)
+                try {
+                    await updateDoc(activityRef, {
+                        applicants: newApplicantsArray
+                    })
+                        .then(() => setParticipateMessage("Votre demande a bien été envoyée, l'organisateur la vérifiera très vite"))
+                }
+                catch (err) {
+                    console.log(err)
+                }
+            }
+
         }
-        catch (err) {
-            console.log(err)
-        }
+
     }
 
     return (
@@ -56,6 +68,7 @@ function Activity({ route, navigation }) {
                 <Button title="Intéressé(e)" />
                 <Button title="Plus" />
             </View>
+            <Text>{participateMessage}</Text>
 
             <Text>{item.activityDescription}</Text>
 
