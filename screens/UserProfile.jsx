c31c7187260f3f6228820ea5caf5797c6058f2e5
Maxence <maxence.califano@gmail.com>
2023-02-13 16:25:14 +0200

    build signOut

--- screens/UserProfile.jsx
+++ screens/UserProfile.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { getAuth, signOut } from "firebase/auth";
 
 
 export default function UserProfile() {
+    const auth = getAuth();
+
     return (
         <View style={{ flex: 1 }}>
             <Text>Page profile</Text>
+            <Button onPress={() => {
+                signOut(auth).then(() => {
+                    // Sign-out successful.
+                }).catch((error) => {
+                    // An error happened.
+                })
+            }
+            } title="Se déconnecter" />
         </View>
     );
 }
