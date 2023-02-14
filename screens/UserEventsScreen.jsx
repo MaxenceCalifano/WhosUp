28b6d49564a0b28e47894cf1fdbfcfdb46cbc3b7
Maxence <maxence.califano@gmail.com>
2023-02-14 11:36:53 +0200

    load activities

--- screens/UserEventsScreen.jsx
+++ screens/UserEventsScreen.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from 'react-native'
+import { useAuthentication } from '../utils/hooks/useAuthentication'
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+
+import app from '../config/firebase';
+import ActivityCard from "../components/ActivityCard";
+
+function UserEventScreen({ navigation }) {
+    //console.log("🚀 ~ file: UserEventsScreen.jsx:10 ~ UserEventScreen ~ user", user.uid)
+    const [test, setTest] = useState([])
+
+    const db = getFirestore(app);
+    const { user } = useAuthentication()
+    const getDocuments = async () => {
+        const q = query(collection(db, "activities"), where("hostId", "==", user.uid));
+
+        const querySnapshot = await getDocs(q);
+        let interTest = []
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            interTest.push(doc.data())
+        });
+        setTest(interTest)
+    }
+    useEffect(() => {
+        if (user) {
+            getDocuments()
+        }
+    }, [user])
 
 
-export default function UserEventScreen() {
     return (
         <View style={{ flex: 1 }}>
             <Text>Mes événements</Text>
+            {test.map((doc, index) => <ActivityCard navigation={navigation} item={doc} />)}
         </View>
+        /**
+         * Charger toutes les activités auquels participe l'utilisateur et celles dont il est
+         */
     );
 }
+
+export default UserEventScreen;
