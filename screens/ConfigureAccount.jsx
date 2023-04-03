a91fd7b622343cef0a5a1ce0706742f1688247d2
Maxence <maxence.califano@gmail.com>
2023-04-03 22:19:56 +0300

    update username in configure account

--- screens/ConfigureAccount.jsx
+++ screens/ConfigureAccount.jsx
@@ -3,22 +3,25 @@ import { StyleSheet, View, TextInput, Text, Pressable, Image, Button } from 'rea
 import * as ImagePicker from 'expo-image-picker';;
 import styles from "../styles";
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { supabase } from '../config/supabase'
+
 
 
 export default function ConfigureAccountScreen({ navigation }) {
     const [name, setName] = useState()
     const [photoURL, setPhoto] = useState(null)
+    const [userId, setUserId] = useState()
+    const [responseMessage, setResponseMessage] = useState('')
+
+    const getCurrentUser = async => {
+        supabase.auth.getSession()
+            .then(res => {
+                setUserId(res.data.session.user.id)
+            })
+    }
 
     useEffect(() => {
-        /*   const storeData = async (value) => {
-              try {
-                  await AsyncStorage.setItem('welcomeScreenSeen', value)
-              } catch (e) {
-                  console.log("🚀 ~ file: ConfigureAccount.jsx:17 ~ storeData ~ e:", e)
-                  // saving error
-              }
-          }
-          storeData("true") */
+        getCurrentUser()
     }, [])
 
     const imagePicker = async () => {
@@ -34,16 +37,33 @@ export default function ConfigureAccountScreen({ navigation }) {
         }
     }
 
-    const updateAccount = () => {
-        /*  .then(() => navigation.navigate('Carte'))
-              .catch(error => console.error(error)) */
+    const updateAccount = async () => {
+        const { error, status } = await supabase
+            .from('profiles')
+            .update({ username: name })
+            .eq('id', userId)
+
+        if (error) setResponseMessage(error)
+        if (status === 204) {
+            setResponseMessage("Votre compte a été mis à jour, redirection vers la page principale")
+            setTimeout(() => navigation.navigate('Carte'), 2000)
+            const storeData = async (value) => {
+                try {
+                    await AsyncStorage.setItem('welcomeScreenSeen', value)
+                } catch (e) {
+                    console.log("🚀 ~ file: ConfigureAccount.jsx:17 ~ storeData ~ e:", e)
+                    // saving error
+                }
+            }
+            storeData("true")
+        }
+
     }
 
     return (
         <View style={styles.authContainer}>
             <Text>Configurer le profile</Text>
             <TextInput placeholder="pseudo" value={name} onChangeText={(value) => setName(value)} />
-            <Text>type de name {typeof name}</Text>
             <Pressable style={[styles.button, { backgroundColor: 'rgba(54,54,54, 0.7)' }]} onPress={() => imagePicker()}>
                 <Text>Choisir une photo</Text>
             </ Pressable>
@@ -55,9 +75,8 @@ export default function ConfigureAccountScreen({ navigation }) {
                     </Pressable>
                 </View>
             }
-
-
             <Button title='valider' onPress={() => updateAccount()} />
+            <Text>{responseMessage}</Text>
         </View>
     )
 }
