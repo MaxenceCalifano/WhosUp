3db3b9926e81ccf1848d381e6f1f318661f7e633
Maxence <maxence.califano@gmail.com>
2023-04-03 21:25:15 +0300

    fix redirect

--- components/Map.jsx
+++ components/Map.jsx
@@ -56,6 +56,7 @@ export default function Map({ navigation }) {
                 const value = await AsyncStorage.getItem("welcomeScreenSeen")
                 if (value === 'false') {
                     // value previously stored
+                    console.log('navigate')
                     navigation.navigate("Configurer le compte")
                 }
             } catch (e) {
@@ -66,7 +67,7 @@ export default function Map({ navigation }) {
         getData()
         fetchData()
         //console.log(activities, 'ligne 52')
-    }, [AsyncStorage])
+    }, [])
 
 
     return (
