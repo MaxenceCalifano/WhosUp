32c3fa40cc9324164c3b64567d9cc52340fdb64f
Maxence <maxence.califano@gmail.com>
2023-04-21 12:30:05 +0300

    rls on messages and fetch messages

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,11 +1,37 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Input, Icon } from '@rneui/themed';
+import { useEffect } from 'react';
+import { supabase } from '../config/supabase'
+import { useState } from 'react';
+
 function Chat({ route }) {
 
-    let roomId = route.params
+    let roomId = route.params.chatId
     console.log("🚀 ~ file: ChatScreen.jsx:6 ~ Chat ~ roomId:", roomId)
+    const [messages, setMessages] = useState([])
+
+    const fetchMessages = async () => {
+
+        const { data, error } = await supabase
+            .from('messages')
+            .select()
+            .eq('chat_room_id', roomId)
+
+        if (data) {
+            console.log('messages', data)
+            setMessages(data)
+        }
+        if (error) console.log(error)
+    }
+
+    useEffect(() => {
+        fetchMessages()
+    }, [])
     return (
         <View>
+            {
+                messages.map(message => <Text key={message.id}>{message.content}</Text>)
+            }
             <Input placeholder='Ecrivez un message...'
                 rightIcon={{ type: 'font-awesome', name: 'paper-plane' }}
             />
