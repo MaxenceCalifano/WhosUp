db7c20edf5684b3f5b42b64aac210eecb7b442da
Maxence <maxence.califano@gmail.com>
2023-04-25 20:36:44 +0300

    message input

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,8 +1,9 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { Input, Icon } from '@rneui/themed';
+import { StyleSheet, View, Text, TextInput, Dimensions } from 'react-native';
 import { useEffect } from 'react';
 import { supabase } from '../config/supabase'
 import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import styles from "../styles";
 
 function Chat({ route }) {
 
@@ -39,15 +40,46 @@ function Chat({ route }) {
         fetchMessages()
     }, [])
     return (
-        <View>
+        <View style={chatStyles.main}>
             {
                 messages.map(message => <Text key={message.id}>{message.content}</Text>)
             }
-            <Input placeholder='Ecrivez un message...'
-                rightIcon={{ type: 'font-awesome', name: 'paper-plane' }}
-            />
+            <View style={chatStyles.send}>
+                <TextInput style={chatStyles.messageInput} placeholder='Ecrivez un message...' />
+                <Ionicons style={chatStyles.send_button} name="send" size={24} color={styles.color} />
+            </View>
         </View>
     );
 }
+
+const chatStyles = StyleSheet.create({
+    main: {
+        height: '100%',
+    },
+    send: {
+        position: 'absolute',
+        bottom: 10,
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    messageInput: {
+        padding: 5,
+        backgroundColor: 'white',
+        //width: '80%',
+        flex: 2,
+        marginRight: 5,
+        borderRadius: 10,
+        elevation: 1
+    },
+    send_button: {
+        backgroundColor: "white",
+        padding: 10,
+        borderRadius: 50,
+        elevation: 1
+    }
+})
 
 export default Chat;
