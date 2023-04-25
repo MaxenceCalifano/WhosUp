1890c0079972e37949f4cda5f91d9834f73afee2
Maxence <maxence.califano@gmail.com>
2023-04-25 19:41:03 +0300

    loader in chatlistscreen

--- screens/ChatListScreen.jsx
+++ screens/ChatListScreen.jsx
@@ -1,40 +1,17 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, Dimensions } from "react-native";
 import ChatRoomListItem from "../components/ChatRoomListItem";
 import { useFocusEffect } from '@react-navigation/native';
 import { useCallback, useState, useEffect } from "react";
 import { supabase } from '../config/supabase'
 import { useUser } from "../UserContext";
+import styles from "../styles";
 
-const data = [
-    {
-        id: 1,
-        content: "coucou",
-        username: 'Jean-Michel',
-        avatarUri: require('../assets/AvatarMaker.png')
-    },
-    {
-        id: 2,
-        content: "Salut, ça va ?",
-        username: 'Jean-Luc',
-        avatarUri: require('../assets/AvatarMaker2.png')
-    },
-    {
-        id: 3,
-        content: "A la prochaine",
-        username: 'Jean-Claude',
-        avatarUri: require('../assets/AvatarMaker3.png')
-    },
-    {
-        id: 4,
-        content: "azertyui",
-        username: 'Jean-Pascal',
-        avatarUri: require('../assets/AvatarMaker4.png')
-    },
-]
+
 function ChatListScreen({ navigation }) {
     const { user } = useUser()
     const [chatUsers, setChatUsers] = useState([])
     const [chatRooms, setChatRooms] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         console.log('chatUsers', chatUsers)
@@ -43,6 +20,7 @@ function ChatListScreen({ navigation }) {
 
     //fetch all the ids of the chatrooms of the logged in user
     const fetchChatroomsIds = async () => {
+        setIsLoading(true)
         const { data, error } = await supabase
             .from('profiles')
             .select(`
@@ -55,7 +33,6 @@ function ChatListScreen({ navigation }) {
 
         if (data) {
             const chat_rooms = data[0].chat_rooms
-            console.log('ligne 55 chatListScreen', data[0].chat_rooms)
             setChatRooms(data[0].chat_rooms)
             chat_rooms.forEach(room => fetchChatUser(room.id))
         }
@@ -74,10 +51,9 @@ function ChatListScreen({ navigation }) {
 
         if (chat_rooms_profiles) {
             chat_rooms_profiles.forEach(item => {
-                console.log("🚀 ~ file: ChatListScreen.jsx:77 ~ fetchChatUser ~ item:", item)
                 if (item.user_id !== user.id) setChatUsers(prevState => [...prevState, { item, roomId: room }])
             })
-
+            setIsLoading(false)
         }
         if (error) console.warn(error)
     }
@@ -91,13 +67,19 @@ function ChatListScreen({ navigation }) {
 
     return (
         <View style={chatStyles.container}>
-            <FlatList
-                data={chatUsers}
-                renderItem={item => <ChatRoomListItem
-                    navigation={navigation}
-                    item={item.item}
-                    keyExtractor={item => item.user_id} />}
-            />
+            {
+                isLoading ? <View style={chatStyles.loaderContainer}>
+                    <ActivityIndicator color={styles.color} size={"large"} />
+                </View>
+                    : <FlatList
+                        data={chatUsers}
+                        renderItem={item => <ChatRoomListItem
+                            navigation={navigation}
+                            item={item.item}
+                            keyExtractor={item => item.user_id} />}
+                    />
+            }
+
         </View>
     );
 }
@@ -105,6 +87,11 @@ function ChatListScreen({ navigation }) {
 const chatStyles = StyleSheet.create({
     container: {
         paddingHorizontal: 10,
+    },
+    loaderContainer: {
+        display: "flex",
+        justifyContent: 'center',
+        height: Dimensions.get('window').height,
     }
 })
 export default ChatListScreen;
