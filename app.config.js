ec8a2ae85dd082f8310191c1ddff775cf3ad12ab
Maxence <maxence.califano@gmail.com>
2023-05-16 22:19:11 +0300

    remove asset from ignore and back to dynamic conf

---
+++ app.config.js
@@ -0,0 +1,40 @@
+export default {
+  expo: {
+    name: "WhosUp",
+    slug: "whosUp",
+    owner : 'velomarchand',
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: './assets/icon.png',
+    userInterfaceStyle: "light",
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    extra: {
+      eas: {
+        projectId: "3e85fa30-fc15-4e46-805e-c1d768961bd7"
+      },
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: 'com.whosup.app',
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    }
+  },
+  assetBundlePatterns: [
+    "**/*",
+  ],
+}
