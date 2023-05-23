bc5fb090bb883fe1bf461a40cd3cc1f0fcd2e820
Maxence <maxence.califano@gmail.com>
2023-05-23 18:11:05 +0300

    splash screen

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ export default {
     splash: {
       image: './assets/splash.png',
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#F5DF4D"
     },
     runtimeVersion: {
       policy: "sdkVersion"
