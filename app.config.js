19c4b378576cd7c3593debe4cea076df5ead7539
Maxence <maxence.califano@gmail.com>
2023-06-05 19:06:54 +0300

    update logo

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     owner : 'velomarchand',
     version: "1.0.0",
     orientation: "portrait",
-    icon: './assets/icon.png',
+    icon: './assets/adaptive-icon.png',
     userInterfaceStyle: "light",
     splash: {
       image: './assets/splash.png',
