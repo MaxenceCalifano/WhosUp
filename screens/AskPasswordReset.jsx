06372f1b4909d16e426ef731da4a46552fd7efa6
Maxence <maxence.califano@gmail.com>
2023-06-26 23:59:25 +0300

    AskPasswordReset

--- screens/AskPasswordReset.jsx
+++ screens/AskPasswordReset.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Button, Icon } from '@rneui/themed';
 import { TextInput } from "react-native-gesture-handler";
 import styles from "../styles";
@@ -8,36 +8,46 @@ import { supabase } from '../config/supabase'
 function AskPasswordReset() {
 
     const [email, setEmail] = useState()
-    const [error, setErrorMessage] = useState()
+    const [response, setResponseMessage] = useState()
 
     const sendResetPasswordEmail = async () => {
         const { data, error } = await supabase.auth
             .resetPasswordForEmail(email, { redirectTo: "http://127.0.0.1:5173/" })
 
         if (data) {
+            setResponseMessage("Vous allez recevoir un e-mail dans de réinitialisation dans quelques instants")
             console.log("🚀 ~ file: PasswordReset.jsx:20 ~ sendResetPasswordEmail ~ data:", data)
-
         }
 
         if (error) {
-            console.log("🚀 ~ file: PasswordReset.jsx:18 ~ sendResetPasswordEmail ~ error:", error)
-            setErrorMessage("Une erreur est survenue")
+            if (error.status === 422) {
+                console.log("🚀 ~ file: PasswordReset.jsx:18 ~ sendResetPasswordEmail ~ error:", error.status)
+                setResponseMessage("L'adresse e-mail est invalide")
+            } else {
+                setResponseMessage("Une erreur est survenue")
+            }
         }
     }
 
     return (
-        <View style={{ flex: 1, alignItems: "stretch", justifyContent: "center", padding: 20, gap: 10 }}>
-            <TextInput placeholder="e-mail" onChangeText={value => setEmail(value)} style={{ backgroundColor: "white", padding: 5 }} />
+        <View style={{ flex: 1, alignItems: "stretch", justifyContent: "center", padding: 20, gap: 30, backgroundColor: styles.color }}>
+            <TextInput placeholder="e-mail" onChangeText={value => setEmail(value)} style={{ backgroundColor: "white", padding: 5, borderRadius: 5 }} />
             <Button
-                radius={'sm'}
                 buttonStyle={{
-                    backgroundColor: styles.color,
+                    backgroundColor: "#454545",
+                    borderRadius: 15,
+                    padding: 15,
+                    gap: 10
+                }}
+                titleStyle={{
+                    color: "white"
                 }}
                 onPress={sendResetPasswordEmail}
             >
                 Réinitialiser le mot de passe
                 <Icon name="send" color="white" />
             </Button>
+            <Text style={{ alignSelf: "center", fontWeight: "bold" }}>{response}</Text>
         </View>
     );
 }
