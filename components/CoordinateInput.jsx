0c659788444203ed626c4583e70e23233d5b9af3
Maxence <maxence.califano@gmail.com>
2023-06-29 18:42:14 +0300

    rm use effect

--- components/CoordinateInput.jsx
+++ components/CoordinateInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet, Modal, Pressable, Dimensions } from 'react-native'
 
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
