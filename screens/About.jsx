57f7968505ddb73429e415ae6a3ff9ec6f2dab3a
Maxence <maxence.califano@gmail.com>
2023-07-10 12:27:26 +0400

    minor fix about

--- screens/About.jsx
+++ screens/About.jsx
@@ -2,16 +2,16 @@ import { View, Text } from "react-native";
 
 function About() {
     return (
-        <View style={{ gap: 50, paddingVertical: 20 }}>
+        <View style={{ gap: 50, padding: 20 }}>
             <Text>
                 Si vous avez des suggestions ou remarques vous pouvez écrire à l'adresse e-mail : maxence.califano@gmail.com
             </Text>
             <View style={{ gap: 10 }}>
                 <Text style={{ fontWeight: "bold" }}>Crédits photos:</Text>
-                <Text>L'image représant de l'activité randonné est de Eric Sanman: https://www.pexels.com/fr-fr/photo/groupe-de-personnes-marchant-en-montagne-1365425/</Text>
-                <Text>L'image représant de l'activité Apéro est de Kindel Media: https://www.pexels.com/fr-fr/photo/toast-soleil-couchant-plage-mains-7148673/</Text>
-                <Text>L'image représant de l'activité Jeux de société est de KoolShooters  : https://www.pexels.com/fr-fr/photo/plage-vacances-sable-amis-8974501/</Text>
-                <Text>L'image représant de l'activité Autre est de Dcstudio : https://fr.freepik.com/auteur/dcstudio</Text>
+                <Text>L'image représant l'activité randonné est de Eric Sanman: https://www.pexels.com/fr-fr/photo/groupe-de-personnes-marchant-en-montagne-1365425/</Text>
+                <Text>L'image représant l'activité Apéro est de Kindel Media: https://www.pexels.com/fr-fr/photo/toast-soleil-couchant-plage-mains-7148673/</Text>
+                <Text>L'image représant l'activité Jeux de société est de KoolShooters  : https://www.pexels.com/fr-fr/photo/plage-vacances-sable-amis-8974501/</Text>
+                <Text>L'image représant l'activité Autre est de Dcstudio : https://fr.freepik.com/auteur/dcstudio</Text>
             </View>
         </View>
     );
