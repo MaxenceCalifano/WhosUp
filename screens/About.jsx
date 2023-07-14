3619a0390accab3b9bf01485b7161513c6d89239
Maxence <maxence.califano@gmail.com>
2023-07-14 08:32:16 +0400

    fix mispelling

--- screens/About.jsx
+++ screens/About.jsx
@@ -8,10 +8,10 @@ function About() {
             </Text>
             <View style={{ gap: 10 }}>
                 <Text style={{ fontWeight: "bold" }}>Crédits photos:</Text>
-                <Text>L'image représant l'activité randonné est de Eric Sanman: https://www.pexels.com/fr-fr/photo/groupe-de-personnes-marchant-en-montagne-1365425/</Text>
-                <Text>L'image représant l'activité Apéro est de Kindel Media: https://www.pexels.com/fr-fr/photo/toast-soleil-couchant-plage-mains-7148673/</Text>
-                <Text>L'image représant l'activité Jeux de société est de KoolShooters  : https://www.pexels.com/fr-fr/photo/plage-vacances-sable-amis-8974501/</Text>
-                <Text>L'image représant l'activité Autre est de Dcstudio : https://fr.freepik.com/auteur/dcstudio</Text>
+                <Text>L'image représentant l'activité randonné est de Eric Sanman: https://www.pexels.com/fr-fr/photo/groupe-de-personnes-marchant-en-montagne-1365425/</Text>
+                <Text>L'image représentant l'activité Apéro est de Kindel Media: https://www.pexels.com/fr-fr/photo/toast-soleil-couchant-plage-mains-7148673/</Text>
+                <Text>L'image représentant l'activité Jeux de société est de KoolShooters  : https://www.pexels.com/fr-fr/photo/plage-vacances-sable-amis-8974501/</Text>
+                <Text>L'image représentant l'activité Autre est de Dcstudio : https://fr.freepik.com/auteur/dcstudio</Text>
             </View>
         </View>
     );
