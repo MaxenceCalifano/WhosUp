377ab4dab358ac79c7b055b912e5e504d463c439
Maxence <maxence.califano@gmail.com>
2023-07-19 18:14:06 +0400

    personal data

--- screens/About.jsx
+++ screens/About.jsx
@@ -3,9 +3,17 @@ import { View, Text } from "react-native";
 function About() {
     return (
         <View style={{ gap: 50, padding: 20 }}>
-            <Text>
-                Si vous avez des suggestions ou remarques vous pouvez écrire à l'adresse e-mail : maxence.califano@gmail.com
-            </Text>
+            <View>
+                <Text style={{ fontWeight: "bold" }}>
+                    Remarques / suggestion :
+                </Text>
+                <Text>
+                    Si vous avez des suggestions ou remarques vous pouvez écrire à l'adresse e-mail : vantivities@gmail.com
+                </Text>
+                <Text style={{ fontWeight: "bold", marginTop: 10 }}>Données personnelles:</Text>
+                <Text>Vantivities ne partage aucune de vos données avec des tiers, si vous souhaitez faire supprimer vos données ou supprimer complètement votre compte, vous pouvez écrire à l'adresse vantivities@gmail.com</Text>
+            </View>
+
             <View style={{ gap: 10 }}>
                 <Text style={{ fontWeight: "bold" }}>Crédits photos:</Text>
                 <Text>L'image représentant l'activité randonné est de Eric Sanman: https://www.pexels.com/fr-fr/photo/groupe-de-personnes-marchant-en-montagne-1365425/</Text>
