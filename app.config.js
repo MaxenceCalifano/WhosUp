f18f7744e2157dbb5d3e30cdd8c3a1351541d3d0
Maxence <maxence.califano@gmail.com>
2023-08-03 15:53:13 +0400

    version

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "Vantivities",
     slug: "whosUp",
     owner : 'velomarchand',
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     userInterfaceStyle: "light",
     scheme :"whosup",
