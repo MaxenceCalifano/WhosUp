9302b84bb3d14fb5b9b5365e0184aed885f31ce0
Maxence <maxence.califano@gmail.com>
2023-08-03 13:48:36 +0400

    add logo

--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -4,11 +4,13 @@ import React from "react";
 import reactDom from 'react-dom';
 import styles from '../styles';
 
-import { StyleSheet, View, Text, Pressable } from 'react-native'
+import { StyleSheet, View, Text, Pressable, Image } from 'react-native'
 
 export default function WelcomeScreen({ navigation }) {
     return (
         <View style={styles.authContainer}>
+            <Image style={style.logo} source={require('../assets/adaptive-icon.png')} />
+            <Text style={{ color: '#454545', fontWeight: 'bold', alignSelf: 'center', fontSize: 30, marginVertical: 0 }}>Vantivities</Text>
             <View style={{ borderBottomColor: 'white', borderBottomWidth: 3, width: '50%', alignSelf: 'center' }} />
             <Text style={{ fontWeight: 'bold', fontSize: 25, alignSelf: 'center', color: '#454545' }}>
                 Rencontrez des voyageurs !
@@ -47,6 +49,19 @@ const style = StyleSheet.create({
     buttonsText: {
         textAlign: 'center',
         fontWeight: '600'
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        alignSelf: 'center'
+    },
+    authContainer: {
+        flex: 1,
+        //justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#F5DF4D',
+        padding: 10,
+        gap: 20
     }
 
 })
