72f1ab36d3d01acbc0ed62ba77b612cf0bb229a9
Maxence <maxence.califano@gmail.com>
2023-08-24 10:58:45 +0400

    update version

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: "Vantivities",
     slug: "whosUp",
     owner : 'velomarchand',
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     userInterfaceStyle: "light",
     scheme :"whosup",
@@ -37,7 +37,7 @@ export default {
     },
     android: {
       package: 'com.vantivities.app',
-      versionCode: 2,
+      versionCode: 3,
       config: {
         googleMaps:{
           apiKey: "AIzaSyA8HqahYLkO53-h022oIQeztlgNvWx_6vg"
