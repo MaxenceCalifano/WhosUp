2e22a2fb6b0367417445af3830564220005e9d1c
Maxence <maxence.califano@gmail.com>
2023-09-13 21:07:50 +0400

    set up secrets

--- app.config.js
+++ app.config.js
@@ -2,57 +2,55 @@ export default {
   expo: {
     name: "Vantivities",
     slug: "whosUp",
-    owner : 'velomarchand',
-    version: "1.0.7",
+    owner: "velomarchand",
+    version: "1.0.8",
     orientation: "portrait",
     userInterfaceStyle: "light",
-    scheme :"whosup",
+    scheme: "whosup",
     splash: {
-      image: './assets/splash.png',
+      image: "./assets/splash.png",
       resizeMode: "contain",
-      backgroundColor: "#F5DF4D"
+      backgroundColor: "#F5DF4D",
     },
     build: {
       production: {
-        channel: "production"
-      }
+        channel: "production",
+      },
     },
     runtimeVersion: {
-      policy: "sdkVersion"
+      policy: "sdkVersion",
     },
     extra: {
       eas: {
-        projectId: "3e85fa30-fc15-4e46-805e-c1d768961bd7"
+        projectId: "3e85fa30-fc15-4e46-805e-c1d768961bd7",
       },
     },
     updates: {
       fallbackToCacheTimeout: 0,
-      url: "https://u.expo.dev/3e85fa30-fc15-4e46-805e-c1d768961bd7"
+      url: "https://u.expo.dev/3e85fa30-fc15-4e46-805e-c1d768961bd7",
     },
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     androidStatusBar: {
       backgroundColor: "#F5DF4D",
     },
     android: {
-      package: 'com.vantivities.app',
-      versionCode: 8,
+      package: "com.vantivities.app",
+      versionCode: 9,
       config: {
-        googleMaps:{
-          apiKey: process.env.EXPO_PUBLIC_MAPS
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_MAPS,
         },
       },
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#F5DF4D"
+        backgroundColor: "#F5DF4D",
       },
     },
     web: {
-      favicon: './assets/favicon.png'
-    }
+      favicon: "./assets/favicon.png",
+    },
   },
-  assetBundlePatterns: [
-    "**/*",
-  ],
-}
+  assetBundlePatterns: ["**/*"],
+};
