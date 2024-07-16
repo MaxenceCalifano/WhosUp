8926ca4529ae4252eb993b7b696d3959db20a1e0
MaxenceCalifano <maxence.califano@gmail.com>
2024-07-16 16:00:10 +0200

    add I18next

---
+++ Translations/index.js
@@ -0,0 +1,21 @@
+import * as resources from "./resources";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: "v3",
+  resources: {
+    ...Object.entries(resources).reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: {
+          translation: value,
+        },
+      }),
+      {}
+    ),
+  },
+  lng: "en",
+});
+
+export default i18n;
